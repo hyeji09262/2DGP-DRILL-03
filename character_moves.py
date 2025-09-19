94b22b53ef17f8b905faf08f188db1a557bfeae6
Hyeji <hyeji09261@tukorea.ac.kr>
2025-09-19 19:35:55 +0900

    사각운동, 원운동 함수뼈대 완성

--- character_moves.py
+++ character_moves.py
@@ -1,8 +1,27 @@
+from shutil import move
+
 from pico2d import *
 
 open_canvas()
 
+
+def move_ractangle():
+    pass
+
+
+
+def move_circle():
+    pass
+
+
+circle()
+
 while True:
+    move_ractangle()
+
+
+
+
     pass
 
 
